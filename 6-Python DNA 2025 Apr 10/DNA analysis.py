1b88548ee2123676978bb41eef2aa7c2fd563ca9
Andrew Lo <andrewlyk@hotmail.com>
2026-02-09 21:40:34 +0800

    dNA matching game
    name	AGATC	TTTTTTCT	AATG	TCTAG	GATA	TATC	GAAA	TCTG
    0	Albus	15	49	38	5	14	44	14	12
    1	Cedric	31	21	41	28	30	9	36	44
    2	Draco	9	13	8	26	15	25	41	39
    3	Fred	37	40	10	6	5	10	28	8
    4	Ginny	37	47	10	23	5	48	28	23
    5	Hagrid	25	38	45	49	39	18	42	30
    6	Harry	46	49	48	29	15	5	28	40
    7	Hermione	43	31	18	25	26	47	31	36
    8	James	46	41	38	29	15	5	48	22
    9	Kingsley	7	11	18	33	39	31	23	14
    10	Lavender	22	33	43	12	26	18	47	41
    11	Lily	42	47	48	18	35	46	48	50
    12	Lucius	9	13	33	26	45	11	36	39
    13	Luna	18	23	35	13	11	19	14	24
    14	Minerva	17	49	18	7	6	18	17	30
    15	Neville	14	44	28	27	19	7	25	20
    16	Petunia	29	29	40	31	45	20	40	35
    17	Remus	6	18	5	42	39	28	44	22
    18	Ron	37	47	13	25	17	6	13	35
    19	Severus	29	27	32	41	6	27	8	34
    20	Sirius	31	11	28	26	35	19	33	6
    21	Vernon	26	45	34	50	44	30	32	28
    22	Zacharias	29	50	18	23	38	24	22	9

---
+++ 6-Python DNA 2025 Apr 10/DNA analysis.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+small_db = pd.read_csv("C:/Users/Lenovo/6-Python DNA 2025 Apr 10/databases/small.csv")  # for code testing
+
+# small_db = pd.read_csv("databases/small.csv")  # for code testing
+
+small_db
